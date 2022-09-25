0d0a45464711388a20516680cd7ac46d0c468938
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-25 19:43:07 -0300

    feat: completed [GET]/users/:user_id route challege

--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -6,8 +6,13 @@ class ShowUserProfileController {
   constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { id } = request.params;
-    return response.json(this.showUserProfileUseCase.execute({ user_id: id }));
+    try {
+      const { user_id } = request.params;
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(200).json(user);
+    } catch (error) {
+      return response.status(404).json({ error: error.message });
+    }
   }
 }
 
