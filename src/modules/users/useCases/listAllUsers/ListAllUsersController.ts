7dd8ca78d11ca878ac99c94385fc4f351a0c73b2
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-25 19:42:25 -0300

    feat: completed [GET]/users route challege

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,11 +6,15 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    const { user_id } = request.headers;
+    try {
+      const { user_id } = request.headers;
 
-    return response.json(
-      this.listAllUsersUseCase.execute({ user_id } as IRequest)
-    );
+      return response.json(
+        this.listAllUsersUseCase.execute({ user_id } as IRequest)
+      );
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
   }
 }
 
