d398f319acf949e3fefda8f56c8dd57e6c0984cf
andreteixeira98 <andreteixeiracbjr@gmail.com>
2022-09-26 18:42:53 -0300

    chore(api docs): setup swagger

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,14 @@
 import express from "express";
+import SwaggerUI from "swagger-ui-express";
 
 import { usersRoutes } from "./routes/users.routes";
+import swaggerDocument from "./swagger.json";
 
 const app = express();
 
 app.use(express.json());
 
 app.use("/users", usersRoutes);
+app.use("/api-docs", SwaggerUI.serve, SwaggerUI.setup(swaggerDocument));
 
 export { app };
